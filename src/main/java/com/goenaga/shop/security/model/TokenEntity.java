8d70378ef9ce8759038ebb91447c9c65ad30911b
Cristian <crisgoera@gmail.com>
2024-10-08 17:17:10 +0200

    Refresh token functionality finished. Allows the last expired jwt token to be used to issue and save new token on db.
    Revokes previous tokens.

--- src/main/java/com/goenaga/shop/security/model/TokenEntity.java
+++ src/main/java/com/goenaga/shop/security/model/TokenEntity.java
@@ -3,12 +3,14 @@ package com.goenaga.shop.security.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("tokens")
 @Builder
 @Getter
 public class TokenEntity {
+    @Indexed(unique = true)
     @NonNull
     private final String email;
     @NonNull
