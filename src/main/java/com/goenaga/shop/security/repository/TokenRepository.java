4cb62a9b2ef864875080ca98c30432602217e1ac
Cristian <crisgoera@gmail.com>
2024-10-09 10:52:04 +0200

    Imports cleanup

--- src/main/java/com/goenaga/shop/security/repository/TokenRepository.java
+++ src/main/java/com/goenaga/shop/security/repository/TokenRepository.java
@@ -4,8 +4,6 @@ import com.goenaga.shop.security.model.TokenEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
 
 
 public interface TokenRepository extends MongoRepository<TokenEntity, String> {
