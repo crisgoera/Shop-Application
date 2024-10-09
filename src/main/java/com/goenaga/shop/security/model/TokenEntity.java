0722c13a0c734ecb88580cdccb3ce15482a0a9b3
Cristian <crisgoera@gmail.com>
2024-10-09 10:53:05 +0200

    Use email as DB id value

--- src/main/java/com/goenaga/shop/security/model/TokenEntity.java
+++ src/main/java/com/goenaga/shop/security/model/TokenEntity.java
@@ -3,14 +3,14 @@ package com.goenaga.shop.security.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
-import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("tokens")
 @Builder
 @Getter
 public class TokenEntity {
-    @Indexed(unique = true)
+    @Id
     @NonNull
     private final String email;
     @NonNull
