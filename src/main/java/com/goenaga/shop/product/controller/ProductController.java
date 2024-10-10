b076b0c5e181f4fc94f2ba657e6a8b292b83dd3a
Cristian <crisgoera@gmail.com>
2024-10-10 09:07:22 +0200

    Initial product controller and service implementation

--- src/main/java/com/goenaga/shop/product/controller/ProductController.java
+++ src/main/java/com/goenaga/shop/product/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.goenaga.shop.product.controller;
 
+import com.goenaga.shop.auth.error.ErrorResponse;
 import com.goenaga.shop.product.model.Product;
 import com.goenaga.shop.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,4 +23,17 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody Product productRequest) {
         return ResponseEntity.ok(productService.createNewProduct(productRequest));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getProductById(@PathVariable int id) {
+        if (productService.getProductById(id).isEmpty()) {
+            ErrorResponse error = ErrorResponse.builder()
+                    .status(HttpStatus.NOT_FOUND)
+                    .message("Resource not found")
+                    .build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        }
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
 }
