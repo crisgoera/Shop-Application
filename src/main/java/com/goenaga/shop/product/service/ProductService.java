30bb9c7ce8117adf4c342de6fe7c18857fc11bfd
Cristian <crisgoera@gmail.com>
2024-10-10 09:06:40 +0200

    Imports clean up

--- src/main/java/com/goenaga/shop/product/service/ProductService.java
+++ src/main/java/com/goenaga/shop/product/service/ProductService.java
@@ -3,10 +3,10 @@ package com.goenaga.shop.product.service;
 import com.goenaga.shop.product.model.Product;
 import com.goenaga.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +17,9 @@ public class ProductService {
 
     public Product createNewProduct(Product productRequest) {
         return productRepository.save(productRequest);
+    }
+
+    public Optional<Product> getProductById(int id) {
+        return productRepository.findProductById(id);
     }
 }
