3994608de2c1e6f76f0562229e52acc606a958a8
Cristian <crisgoera@gmail.com>
2024-10-10 09:07:22 +0200

    Initial product controller and service implementation

--- src/main/java/com/goenaga/shop/product/model/Product.java
+++ src/main/java/com/goenaga/shop/product/model/Product.java
@@ -2,17 +2,29 @@ package com.goenaga.shop.product.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Builder
 @Getter
+@Setter
 @Document("products")
 public class Product {
+    @Id
     private final int productId;
+    @Indexed(unique = true)
+    @NonNull
     private final String name;
     private final String description;
+    @NonNull
     private final float price;
     private final List<String> imgUrls;
+    private final int stock;
+    private final boolean isAvailable;
+    private final int unitsSold;
 }
