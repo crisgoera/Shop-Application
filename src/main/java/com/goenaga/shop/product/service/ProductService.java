7a425d297dcd23c0c2ec259745d286a04458db45
Cristian <crisgoera@gmail.com>
2024-10-19 20:55:15 +0200

    Added logic for createNewItem on the product service

--- src/main/java/com/goenaga/shop/product/service/ProductService.java
+++ src/main/java/com/goenaga/shop/product/service/ProductService.java
@@ -4,7 +4,6 @@ import com.goenaga.shop.product.model.Product;
 import com.goenaga.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -16,10 +15,21 @@ public class ProductService {
     public List<Product> getProducts() { return productRepository.findAll(); }
 
     public Product createNewProduct(Product productRequest) {
-        return productRepository.save(productRequest);
+        Product newProduct = Product.builder()
+                .productId(productIdSequencer())
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .imgUrls(productRequest.getImgUrls())
+                .build();
+        return productRepository.save(newProduct);
     }
 
     public Optional<Product> getProductById(int id) {
         return productRepository.findProductById(id);
+    }
+
+    public int productIdSequencer() {
+        return getProducts().size() + 1;
     }
 }
