52d41d118410fda91a80101840f46a206afa4ec3
Cristian <crisgoera@gmail.com>
2024-10-27 18:09:27 +0100

    Added update product functionality

--- src/main/java/com/goenaga/shop/product/service/ProductService.java
+++ src/main/java/com/goenaga/shop/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.goenaga.shop.product.service;
 
+import com.goenaga.shop.product.mapper.service.MapperService;
 import com.goenaga.shop.product.model.NewProductDTO;
 import com.goenaga.shop.product.model.Product;
 import com.goenaga.shop.product.repository.ProductRepository;
@@ -9,11 +10,13 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
     private final PhotoService photoService;
+    private final MapperService mapperService;
 
     public List<Product> getProducts() { return productRepository.findAll(); }
 
@@ -23,17 +26,28 @@ public class ProductService {
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
-                .productImgs(photoService.addPhotos(productRequest.getImageTitles(), productRequest.getImages()))
+                .photos(photoService.processPhotos(productRequest.getImageTitles(), productRequest.getImages()))
                 .stock(productRequest.getStock())
                 .build();
         return productRepository.save(newProduct);
     }
 
-    public Optional<Product> getProductById(int id) {
-        return productRepository.findProductById(id);
+    public Optional<Product> getProductById(String id) {
+        return productRepository.findById(id);
     }
 
-    public int productIdSequencer() {
-        return getProducts().size() + 1;
+    public Product updateProduct(String id, Product updateDetails) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            Product updatedProduct = mapperService.updateProductDetails(product.get(), updateDetails);
+            return productRepository.save(updatedProduct);
+        }
+
+        return null;
+    }
+
+    public String productIdSequencer() {
+        return String.valueOf(getProducts().size() + 1);
     }
 }
