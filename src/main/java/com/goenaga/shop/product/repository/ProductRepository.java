86df51566948a74f84e55dca00616408356d3cbb
Cristian <crisgoera@gmail.com>
2024-10-27 18:09:04 +0100

    Import clean up

--- src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
+++ src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
@@ -2,9 +2,6 @@ package com.goenaga.shop.product.repository;
 
 import com.goenaga.shop.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.Optional;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
