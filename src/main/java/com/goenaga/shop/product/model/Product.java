e5cd4f6dce94e4b60301148a6c45cce4a9f280f4
Cristian <crisgoera@gmail.com>
2024-10-29 12:52:27 +0100

    Added java util currency to product model. Change price variable type to double instead of int

--- src/main/java/com/goenaga/shop/product/model/Product.java
+++ src/main/java/com/goenaga/shop/product/model/Product.java
@@ -24,7 +24,7 @@ public class Product {
     private String name;
     private String description;
     @NonNull
-    private float price;
+    private double price;
     private Currency currency;
     private List<Photo> photos;
 }
