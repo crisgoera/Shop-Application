6c4094d5ad2c83146f50f6cb2d75e14c150c77c1
Cristian <crisgoera@gmail.com>
2024-10-30 10:48:04 +0100

    Added user mapper for user detail update functionality

--- src/main/java/com/goenaga/shop/product/mapper/service/MapperService.java
+++ src/main/java/com/goenaga/shop/product/mapper/service/MapperService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goenaga.shop.product.model.Product;
+import com.goenaga.shop.user.model.User;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 
@@ -19,5 +20,16 @@ public class MapperService {
         updateMap.forEach((key, value) -> productMap.put(key, value));
 
         return mapper.convertValue(productMap, Product.class);
+    }
+
+    public User updateUserDetails (User user, User updateDetails) {
+        ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        Map<String, Object> updateMap = mapper.convertValue(updateDetails, new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> userMap = mapper.convertValue(user, new TypeReference<Map<String, Object>>() {});
+
+        updateMap.forEach((key, value) -> userMap.put(key, value));
+
+        return mapper.convertValue(userMap, User.class);
     }
 }
