67b480f62aa9e3b0a363d635958c1c570470679d
Cristian <crisgoera@gmail.com>
2024-11-26 12:57:59 +0100

    Added query function

--- src/main/java/com/goenaga/shop/security/repository/TokenRepository.java
+++ src/main/java/com/goenaga/shop/security/repository/TokenRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository("tokenRepository")
 public interface TokenRepository extends CrudRepository<TokenEntity, String> {
+    Optional<TokenEntity> findByUserId(UUID user_Id);
+    void deleteByUserId(UUID user_Id);
 }
