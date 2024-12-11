d44143607f9cc3a3d2d01efd5c353f4c7f59cb68
Cristian <crisgoera@gmail.com>
2024-12-11 20:24:53 +0100

    Testing init for User Service

---
+++ src/test/java/com/goenaga/shop/user/service/UserServiceTest.java
@@ -0,0 +1,68 @@
+package com.goenaga.shop.user.service;
+
+import com.goenaga.shop.auth.model.SignupRequest;
+import com.goenaga.shop.security.service.JWTService;
+import com.goenaga.shop.user.enums.Role;
+import com.goenaga.shop.user.model.User;
+import com.goenaga.shop.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private JWTService jwtService;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void userService_CreateUser_ReturnsCorrectUserInstance() {
+        SignupRequest request = SignupRequest.builder()
+                .email("test@mail.com")
+                .firstName("testUser")
+                .lastName("testLastName")
+                .password("testPassword")
+                .build();
+
+        User newUser = userService.createNewUser(request);
+
+        Assertions.assertThat(newUser).isNotNull();
+        Assertions.assertThat(newUser.getEmail()).isEqualTo(request.getEmail());
+        Assertions.assertThat(newUser.getFirstName()).isEqualTo(request.getFirstName());
+        Assertions.assertThat(newUser.getLastName()).isEqualTo(request.getLastName());
+        Assertions.assertThat(newUser.getPassword()).isNotEqualTo(request.getPassword());
+        Assertions.assertThat(newUser.getCreatedAt()).isNotNull();
+        Assertions.assertThat(newUser.getLastModified()).isNotNull();
+    }
+
+    @Test
+    void getUserFromToken_Returns_OptionalOfUserDTO() {
+        User user = User.builder()
+                .email("test@user")
+                .password("test")
+                .createdAt(new Date())
+                .lastModified(new Date())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+    }
+
+    @Test
+    void updateUserProfile() {
+    }
+}
