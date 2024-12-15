050f0959f84e2a1b63b325dc137c2bb6c386e7ce
Cristian <crisgoera@gmail.com>
2024-12-15 13:12:29 +0100

    Switched default query method to return a List object

--- src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
+++ src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
@@ -2,9 +2,10 @@ package com.goenaga.shop.product.repository;
 
 import com.goenaga.shop.product.model.Product;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;;
+import org.springframework.stereotype.Repository;;import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, String> {
-    Product findTopByOrderByProductIdDesc();
+    @Override
+    List<Product> findAll();
 }
