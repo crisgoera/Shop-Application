afd5257de94e914e93f536059b4125a249cc09c3
Cristian <crisgoera@gmail.com>
2024-12-22 12:12:17 +0100

    import cleanup

--- src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
+++ src/main/java/com/goenaga/shop/product/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package com.goenaga.shop.product.repository;
 
 import com.goenaga.shop.product.model.Product;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;;import java.util.List;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, String> {
