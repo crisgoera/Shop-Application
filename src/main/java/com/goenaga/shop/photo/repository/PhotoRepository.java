860534764aeda4bfa5aba7e05fb551a688084442
Cristian <crisgoera@gmail.com>
2025-07-06 10:37:54 +0200

    Removed redundant repository

--- src/main/java/com/goenaga/shop/photo/repository/PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goenaga.shop.photo.repository;
-
-import com.goenaga.shop.photo.model.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PhotoRepository extends JpaRepository<Photo, UUID> {
-}
